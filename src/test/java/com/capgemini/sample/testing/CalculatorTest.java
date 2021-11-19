36f6583a35a00343d2946c31215e7d1deb38ce26
Maciej Lipski <maciej.lipski@capgemini.com>
2021-11-19 16:21:41 +0100

    Adding Calculator

---
+++ src/test/java/com/capgemini/sample/testing/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.capgemini.sample.testing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CalculatorTest {
+
+	private Calculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new Calculator();
+	}
+
+	@Test
+	void shouldReturn4_whenAdding2And2() {
+		// given
+		int firstNumber = 2;
+		int secondNumber = 2;
+		// when
+		int result = calculator.add(firstNumber, secondNumber);
+		// then
+		assertEquals(4, result);
+	}
+
+	@Test
+	void standardAssertions() {
+		assertEquals(4, calculator.add(1, 3));
+		assertEquals(8, calculator.multiply(2, 4));
+		assertTrue(calculator.add(1, 3) < calculator.multiply(2, 3));
+	}
+
+}
